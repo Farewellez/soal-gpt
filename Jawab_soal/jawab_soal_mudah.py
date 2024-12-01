def58d224f9784460c91991f8fc28d62978973df
Farewellez <ahmadzafarell55@gmail.com>
2024-12-02 00:16:43 +0700

    Udah selesai semua nih level easy 1-20 nya :3

--- Jawab_soal/jawab_soal_mudah.py
+++ Jawab_soal/jawab_soal_mudah.py
@@ -134,3 +134,116 @@ def no10():
             break
     clear_terminal()
     print("Halo,",nama_user + "!","Kamu berusia",usia_tanpa_spasi,"tahun.")
+# No.11
+def no11():
+    while True:
+        try:
+            angka_random = float(input("Masukkan sebuah angka: "))
+            angka_random2 = float(input("Masukkan angka lagi: "))
+            clear_terminal()
+            print(f"Penjumlan dari kedua input angka adalah = {angka_random + angka_random2}")
+            break
+        except ValueError:
+            clear_terminal()
+            print("Masukkan hanya angka!!!")
+            continue
+# No.12
+def no12():
+    while True:
+        try:
+            angka1 = int(input("Masukkan sebuah bilangan bulat: "))
+            angka2 = int(input("Masukkan bilangan bulat kedua: "))
+            break
+        except ValueError:
+            clear_terminal()
+            print("Input hanya berupa bilangan bulat, tanpa spasi dan hanya angka!!!")
+            continue
+    if angka1 > angka2:
+        clear_terminal()
+        print("Angka pertama adalah",angka1,"dan angka kedua adalah",angka2)
+        print(angka1 > angka2,f"karena {angka1} lebih dari {angka2}")
+    else:
+        clear_terminal()
+        print("Angka pertama adalah",angka1,"dan angka kedua adalah",angka2)
+        print(angka1 > angka2,f"karena {angka1} tidak lebih dari {angka2}")
+# No.13
+def no13():
+    while True:
+        try:
+            angka_random = float(input("Masukkan sebuah angka: "))
+            angka_random2 = float(input("Masukkan angka lagi: "))
+            clear_terminal()
+            print(f"Pembagian dari kedua input angka adalah = {angka_random // angka_random2}")
+            break
+        except ValueError:
+            clear_terminal()
+            print("Masukkan hanya angka!!!")
+            continue
+# No.14
+def no14():
+    while True:
+        try:
+            variabel = int(input("Mau melakukan perulangan berapa kali???: "))
+            break
+        except ValueError:
+            print("Masukkan hanya bilangan bulat tanpa spasi!!!")
+            continue
+    for i in range(variabel):
+        print("Hello")
+# No.15
+def no15():
+    π = 22/7
+    while True:
+        try:
+            r = float(input("Masukkan nominal radius lingkaran: "))
+            break
+        except ValueError:
+            print("Masukkan hanya angka tanpa spasi!!!")
+            continue
+    print(f"Luas lingkaran adalah πr² = π dikali {r}² = {π*r**2}")
+# No.16
+def no16():
+    print(f'''
+        Variabel 1 = {"ayam",type('ayam')} ini string 
+        Variabel 2 = {1,type(1)} ini integer
+        Variabel 3 = {1.0,type(1.0)} ini float
+        Variabel 4 = {True and False,type(False)} ini boolean
+        Variabel 5 = {1j,type(1j)} ini complex number''')
+# No.17
+def no17():
+    while True:
+        angka1 = str(input("Masukkan angka 1: "))
+        angka2 = str(input("Masukkan angka 2: "))
+        if not angka1.isdigit and angka2.isdigit:
+            print("Masukkan hanya sebuah angka tanpa spasi")
+            continue
+        else:
+            break
+    clear_terminal()
+    print(f"Angka 1 = {angka1,type(angka1)}\nAngka 2 = {angka2,type(angka2)}")
+    awal = 0
+    akhir = 0
+    for angka in angka1:
+        awal = awal*10 + (ord(angka) - ord('0'))
+    for char in angka2:
+        akhir = akhir*10 + (ord(char) + ord('0'))
+    print(f"sekarang...{awal,type(awal)} dan {akhir,type(akhir)} berubah menjadi integer")
+# No.18
+def no18():
+    text = "Ini String"
+    print(type(text),"Ini adalah string")
+# No.19
+def no19():
+    a = True
+    b = False
+    print(
+        f'''
+        {a and b} = {a} dan {b}
+        {a or b} = {a} atau {b}
+        {a ^ b} = {a} XOR {b}   
+        {a ^ a} = {a} XOR {a}
+        ''')
+# No.20
+def no20():
+    text = "Python"
+    print(f"Karakter pertama dan terakhir dari {text} adalah {text[0:6:5]}")
