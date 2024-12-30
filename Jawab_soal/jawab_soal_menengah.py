847ba3e4aa340c601b96f6b33631aa86238198b1
Farewellez <ahmadzafarell55@gmail.com>
2024-12-30 21:02:21 +0700

    Finished No.27

--- Jawab_soal/jawab_soal_menengah.py
+++ Jawab_soal/jawab_soal_menengah.py
@@ -69,3 +69,58 @@ def no22():
         break
     return
 # No.23
+def no23():
+    code_name = "Isla"
+    reverse = code_name[::-1]
+    return reverse
+# No.24
+def no24():
+    print("Menghitung angka 1 hingga n buah angka: ")
+    n = int(input("Masukkan angka n: "))
+    while n <= 0:
+        print("Maaf, n harus lebih besar dari 0")
+        continue
+    total = 0
+    for i in range (1, n + 1):
+        total += i
+    print(total)
+# No.25
+def no25():
+    print("Masukkan 3 buah angka: ")
+    while True:
+        try:
+            angka1 = int(input("Masukkan angka pertama: "))
+            angka2 = int(input("Masukkan angka kedua: "))
+            angka3 = int(input("Masukkan angka ketiga: "))
+        except ValueError:
+            print("Masukkan hanya angka!")
+            continue
+        break
+    average = (angka1 + angka2 + angka3) / 3
+    averagee = (angka1 + angka2 + angka3) // 3
+    print(f"Rata-rata dari 3 angka tersebut adalah:\n{angka1} + {angka2 } + {angka3} : 3 = {average}")
+    print(f"Jika dibulatkan adalah = {averagee}")
+    return
+# No.26
+def no26():
+    while True:
+        try:
+            celcius = int(input("Input sebuah suhu (°C): "))
+            print("Suhu akan diubah ke fahrenheit (°F)")
+        except ValueError:
+            print("Masukkan hanya angka!")
+            continue
+        break
+    print(f"Suhu {celcius}°C = {celcius * 9/5 + 32}°F")
+    print("FYI 1°C = 1,8°F dan 0°C = 32°F")
+# No.27
+def no27():
+    import random
+    import string
+    random_letter = random.choice(string.ascii_letters)
+    print(f"Huruf acak = {random_letter}")
+    input_user = input("Masukkan sebuah kata acak yang anda inginkan:\nKata Acak: ")
+    print(f"Huruf acak yang anda inginkan = {input_user}")
+    output = random_letter + input_user[1::]
+    print(f"Karakter pertama dari {input_user} ditukar dengan {random_letter}")
+    print(f"Menghasilkan : {output}")
